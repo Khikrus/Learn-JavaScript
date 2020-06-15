98746d132aa06237d65c6baaeda81e5ef49b3df1
Rustem <redzhik@gmail.com>
2020-06-16 02:08:55 +0300

    Добавлены изображения

--- Flappy Bird (game)/js/game.js
+++ Flappy Bird (game)/js/game.js
@@ -1,1 +1,36 @@
-alert('df');
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeUp = new Image();
+var pipeBottom = new Image();
+
+//Изображения
+bird.src = "img/bird.png";
+bg.src = "img/bg.png";
+fg.src = "img/fg.png";
+pipeUp.src = "img/pipeUp.png";
+pipeBottom.src = "img/pipeBottom.png";
+
+//Звуковые файлы
+
+//Растояние между препятствиями
+var gap = 90;
+
+//Позиция bird
+var xPos = 10;
+var yPos = 150;
+
+function draw() {
+    ctx.drawImage(bg, 0, 0);
+
+    ctx.drawImage(pipeUp, 100, 0);
+    ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
+
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
+    ctx.drawImage(bird, xPos, yPos);
+}
+
+pipeBottom.onload = draw;
